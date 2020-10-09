c289dd8b5bc16845255966f4cda56f531c0ae75f
Nomi77 <naeemtahir775@gmail.com>
2020-10-09 13:52:30 +0500

    Update and DeleteMe routed added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const {signup,login,forgotPassword} = require('../controllers/authController');
+const {signup,login,forgotPassword,updatePassword,protect} = require('../controllers/authController');
+const {updateMe,deleteMe,getAllUsers} = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
-router.post('/forgotPassword', forgotPassword);
+// router.post('/forgotPassword', forgotPassword);
+router.patch('/updatePassword',protect,updatePassword);
+
+router.patch('/updateMe', protect, updateMe);
+router.delete('/deleteMe', protect, deleteMe);
+router.route('/').get(getAllUsers);
 module.exports = router;
